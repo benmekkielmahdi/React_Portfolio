5c14bdeb856c0966ad4d1bf28c001b0ab1a923ec
ben mekki el mahdi <95963227+benmekkielmahdi@users.noreply.github.com>
2025-11-19 03:03:04 +0100

    first commit

---
+++ src/Details.js
@@ -0,0 +1,144 @@
+// Enter all your detials in this file
+// Logo images
+// import logogradient from "./assets/logo.svg";
+// import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+import java from "./assets/techstack/java.png";
+import spring from "./assets/techstack/spring.png";
+import spring_boot from "./assets/techstack/spring_boot.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+
+// Logos
+// export const logos = {
+//   logogradient: logogradient,
+//   logo: logo,
+// };
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "El mahdi BEN MEKKI",
+  tagline: "Software Developer",
+  img: profile,
+  about: `El mahdi ben mekki , 22 years old, I am an enthusiastic and self-motivated Full Stack Web Developer
+with a solid foundation in web development principles and a passion for coding. Recently graduated
+with a degree in Computer Science, I have honed my skills in both front-end and back-end
+technologies through various academic projects and self-initiated learning.
+I am proficient in HTML, CSS, JavaScript, React, Node.js, and Python, among other technologies.
+My experience includes building responsive websites, developing dynamic web applications, and
+working with databases to manage and retrieve data efficiently. I am dedicated to writing clean,
+maintainable code and am always eager to learn and adopt new technologies.
+Throughout my academic journey, I have successfully completed several projects that demonstrate
+my ability to design, develop, and deploy web applications. I thrive in collaborative environments
+and am committed to continuous learning and improvement.
+As I embark on my professional career, I am excited to bring my enthusiasm for web development
+and problem-solving skills to a dynamic team. I am eager to contribute to innovative projects, gain
+hands-on experience, and grow as a developer. My goal is to create impactful, user-centric web
+applications that solve real-world problems and enhance user experiences.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://www.github.com/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+
+// Enter your Education Details here
+export const eduDetails = [
+
+  {
+    Position: "Master's degree ",
+    Company: `ENS`,
+    Location: "Marrakech",
+    Type: "Full Time",
+    Duration: "2024 - Present",
+  },
+  {
+    Position: "Bachelor in Computer Science",
+    Company: "ENS",
+    Location: " Meknes",
+    Type: "Full Time",
+    Duration: " 2021 - 2024",
+  },
+ 
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  java: java,
+  spring: spring,
+  spring_boot: spring_boot,
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Library - Online Library Management System",
+    image: projectImage1,
+    description: `An online library management system that allows efficient handling of books, loans, and user accounts. Built with Spring Boot and Thymeleaf for a dynamic and responsive interface.`,
+    techstack: "Spring Boot/Thymeleaf",
+    previewLink: "https://youtu.be/EBNgXmqv0vU?si=3ve187aqz7J-w8G1",
+    githubLink: "https://github.com/benmekkielmahdi/Mini_Projet_Thymeleaf",
+  },
+  {
+    title: "Static Website",
+    image: projectImage2,
+    description: `A static website designed to present information clearly and attractively. Built with HTML, CSS, and JavaScript for smooth and responsive navigation.`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Portfolio",
+    image: projectImage3,
+    description: `A personal portfolio showcasing my skills, projects, and experiences. Developed with HTML, CSS, and JavaScript, optimized for responsive design and excellent user experience.`,
+    techstack: "React, Tailwind",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+   
+ 
+
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "benmekkielmahdi@gmail.com",
+  phone: "+212 770837707",
+};
